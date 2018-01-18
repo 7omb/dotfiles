d678bfc45571193ca7799f4c938327f67bb92c99
Tom Baerwinkel <tb@mathias-kettner.de>
2018-01-18 14:54:53 +0100

    Use points instead of pixels

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -147,7 +147,7 @@ values."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
+                               :size 10.0
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
@@ -323,7 +323,6 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  (spacemacs/toggle-fill-column-indicator-on)
   (setq python-shell-interpreter "python")
   (setq flycheck-checker-error-threshold 2000)
   )
