02cadebcc624650f2c7bb556e71db5afa48aa2ee
Tom Baerwinkel <tb@mathias-kettner.de>
2019-01-22 09:47:16 +0100

    Use default Spacemacs theme

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -72,7 +72,6 @@ values."
                       version-control-diff-side 'left)
      evil-commentary
      evil-snipe
-     themes-megapack
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -150,8 +149,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(sanityinc-solarized-dark
-                         sanityinc-solarized-light)
+   dotspacemacs-themes '(spacemacs-dark
+                         spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
