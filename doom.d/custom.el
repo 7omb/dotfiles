1190813ee9b8506f63c396884fd1f07de37c011f
Tom Baerwinkel <tom.baerwinkel@tribe29.com>
2021-02-09 08:35:16 +0100

    Update safe-local-variable-values

--- doom.d/custom.el
+++ doom.d/custom.el
@@ -4,7 +4,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((py-indent-offset . 4)
+   '((grep-find-ignored-directories ".git" ".venv" "node_modules" ".mypy_cache" ".pytest_cache" "SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}")
+     (grep-find-ignored-files "*.min.js" "*.standalone.js" "*.map" "*.gz" ".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo")
+     (eval add-to-list 'grep-find-ignored-directories ".pytest_cache")
+     (eval add-to-list 'grep-find-ignored-directories ".mypy_cache")
+     (eval add-to-list 'grep-find-ignored-directories "node_modules")
+     (eval add-to-list 'grep-find-ignored-directories ".venv")
+     (eval add-to-list 'grep-find-ignored-directories ".git")
+     (eval add-to-list 'grep-find-ignored-files "*.gz")
+     (eval add-to-list 'grep-find-ignored-files "*js.map")
+     (eval add-to-list 'grep-find-ignored-files "*standalone.js")
+     (eval add-to-list 'grep-find-ignored-files "*min.js")
+     (py-indent-offset . 4)
      (encoding . utf-8)
      (eval setq flycheck-python-mypy-executable
            (concat
