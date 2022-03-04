595726a1e984543755658aafccab0c089593fbea
Tom Bärwinkel <tom.baerwinkel@2xideas.com>
2022-03-04 08:58:14 +0100

    Remove duplicate .local/bin and remove zeug_cmk from PATH

--- oh-my-zsh-custom/machines.zsh
+++ oh-my-zsh-custom/machines.zsh
@@ -6,6 +6,4 @@ if type go > /dev/null; then
     export PATH=$PATH:$(go env GOPATH)/bin
 fi
 
-[ -d "$HOME/.local/bin" ] && export PATH="$HOME/.local/bin:$PATH"
 [ -d "$HOME/.cargo/bin" ] && export PATH="$HOME/.cargo/bin:$PATH"
-[ "$(hostname)" = "klappschloss" ] && export PATH="$PATH:$HOME/git/zeug_cmk/bin"
