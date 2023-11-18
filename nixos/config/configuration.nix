cfe98f84bae759f5df420eec9343dbd97925562f
Tom Bärwinkel <dev@baerwinkel.org>
2023-11-18 22:04:54 +0100

    Enable NVIDIA graphics

--- nixos/config/configuration.nix
+++ nixos/config/configuration.nix
@@ -27,7 +27,7 @@
 
   boot = {
     # Use latest kernel to make wifi work with Intel AX201
-    kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_latest;
 
     tmp.useTmpfs = true;
 
@@ -111,21 +111,55 @@
   fonts.fonts = with pkgs; [ source-code-pro ];
 
   # NVIDIA graphics drivers are currently not working retry later:
-  # https://nixos.wiki/wiki/Nvidia#booting_with_an_external_display
-  #environment.systemPackages = [(pkgs.writeShellScriptBin "nvidia-offload" ''
-  #  export __NV_PRIME_RENDER_OFFLOAD=1
-  #  export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-  #  export __GLX_VENDOR_LIBRARY_NAME=nvidia
-  #  export __VK_LAYER_NV_optimus=NVIDIA_only
-  #  exec -a "$0" "$@"
-  #'')];
-  #services.xserver.videoDrivers = [ "nvidia" ];
-  ##hardware.nvidia.modesetting.enable = true;
-  #hardware.nvidia.prime = {
-  #  offload.enable = true;
-  #  intelBusId = "PCI:0:2:0";
-  #  nvidiaBusId = "PCI:1:0:0";
-  #};
+  # https://nixos.wiki/wiki/Nvidia
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      # Option A: Sync Mode
+      # sync.enable = true;
+      # Option B: Offload Mode
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+  # Option B: Offload Mode
+  environment.systemPackages = [(pkgs.writeShellScriptBin "nvidia-offload" ''
+   export __NV_PRIME_RENDER_OFFLOAD=1
+   export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+   export __GLX_VENDOR_LIBRARY_NAME=nvidia
+   export __VK_LAYER_NV_optimus=NVIDIA_only
+   exec -a "$0" "$@"
+  '')];
 
   # Enable automatic updates
   system.autoUpgrade = {
